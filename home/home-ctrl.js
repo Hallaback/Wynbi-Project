4a3c56c52a871025e8389ab9054c62c6afc1a568
Ryan Hall <ryan.hall0326@gmail.com>
2015-11-03 16:59:35 -0700

    changing the uri redirect

--- home/home-ctrl.js
+++ home/home-ctrl.js
@@ -1,6 +1,6 @@
 app.controller('home-ctrl', ['$scope', 'jobs', function($scope, jobs) {
 	jobs.getJobs().then(function(data) {
-		$scope.jobs = data.data.response;
+		$scope.jobs = data.data.data;
 	});
 	console.log($scope.jobs);
 

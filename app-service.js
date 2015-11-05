bf78f652150fa3a358d4b8510613e723fef306c4
Ryan Hall <ryan.hall0326@gmail.com>
2015-11-05 12:02:19 -0700

    wrote search function based on keywords, styled the site, added service to get one job by id

--- app-service.js
+++ app-service.js
@@ -1,8 +1,12 @@
 app.service('jobs', function($http) {
 
-	this.getJobs = function() {
-		return $http.jsonp('https://api.usa.gov/jobs/search.json?query=nursing+jobs?callback=JSON_CALLBACK');
+	this.getJobs = function(keywords) {
+		return $http.jsonp('https://api.usa.gov/jobs/search.json?query=' + keywords);
 	}
 	console.log(this.getJobs());
+
+	this.getOneJob = function(id) {
+		return $http.jsonp('https://api.usa.gov/jobs/search.json?' + id);
+	}
 
 });
--- home/home-ctrl.js
+++ home/home-ctrl.js
@@ -1,11 +1,12 @@
 app.controller('home-ctrl', ['$scope', 'jobs', function($scope, jobs) {
-	jobs.getJobs().then(function(response) {
-		$scope.jobs = response.data;
-	});
-	console.log($scope.jobs);
+
+	$scope.keywords = " ";
+
+	$scope.searchJobs = function(keywords) {
+		jobs.getJobs($scope.keywords).then(function(data) {
+			$scope.jobs = data.data;
+		});
+		console.log($scope.jobs);
+	};
 
 }]);
-	// $scope.searchJobs = function(keywords) {
-	// 	var newKeywords = keywords.split(' ').join('+');
-	// 	$scope.jobs.search("newKeywords");
-	// };
